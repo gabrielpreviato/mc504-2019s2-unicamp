98fad9dc966d4322faefeb543e318bec558ed341
Gabriel Previato <gabriel.previato@gmail.com>
2019-10-26 11:12:46 -0300

    Pagemap parsing done, need tests

--- exp02/src/Pagemap.cpp
+++ exp02/src/Pagemap.cpp
@@ -1,5 +1,6 @@
 #include "Pagemap.h"
 #include <fstream>
+#include <sstream>
 #include <cstdio>
 #include <unistd.h>
 #include <fcntl.h>
@@ -8,8 +9,10 @@
 
 
 Pagemap::Pagemap(int pid) : _pid(pid), _maps(pid), 
-                            _file_stream(open((std::string("/proc/") + std::to_string(_pid) + std::string("/pagemap")).c_str(), O_RDONLY))
-                            {}
+                            _file_stream(open((std::string("/proc/") + std::to_string(_pid) + std::string("/pagemap")).c_str(), O_RDONLY)) {
+    
+    std::cout << _file_stream << std::endl;
+}
 
 void Pagemap::read_pagemap_file() {
     std::cout << "Before reading maps." << std::endl;
@@ -19,24 +22,42 @@ void Pagemap::read_pagemap_file() {
 
     std::cout << "Done reading maps." << std::endl;
 
+    std::cout << "PAGE SIZE: " << PAGE_SIZE << std::endl;
     
     for (auto range : _maps._maps_virtual_ranges) {
         for (uint64_t i = range.first; i < range.second; i += PAGE_SIZE) {
-            uint64_t page_index = i / PAGE_SIZE;
-            uint64_t page_frame;
+            std::cout << "i: " << i << ", range: " << range.first << '-' << range.second << std::endl;
             
-            if (pread(_file_stream, &page_frame, sizeof(page_frame), page_index) != sizeof(page_frame)) {
+            uint64_t page_frame;
+            uint64_t page_index = i / PAGE_SIZE * sizeof(page_frame);
+
+            std::cout << "page index: " << page_index << std::endl;
+            
+            int ret = pread(_file_stream, &page_frame, sizeof(page_frame), page_index);
+            if (ret == -1) {
                 exit(-1);
             }
+            else if (ret == 0) {
+                std::cout << "EOF" << std::endl;    
+            }
+            else if (ret = sizeof(page_frame)) {
+                std::stringstream sstream;
+                sstream << std::hex << page_frame;
+                std::string result = sstream.str();
 
-            std::cout << page_frame << std::endl;   
+                std::cout << "Page frame: 0x" << result << std::endl;
+            }
+            else {
+                exit(-2);
+            }
         }
     }
+
     return;
 }
 
 void Pagemap::print_pagemap() {
-    _maps.print_maps();
+    // _maps.print_maps();
     // std::cout << _pagemap_file << std::endl;
     
     return;
