20e439eff3140aa7a93ca71d9e23df4f45690078
Gabriel Previato <gabriel.previato@gmail.com>
2019-10-27 18:41:15 -0300

    Finished pagemap parsing

--- exp02/src/Pagemap.cpp
+++ exp02/src/Pagemap.cpp
@@ -8,9 +8,9 @@
 #define PAGE_SIZE 0x1000
 
 
-Pagemap::Pagemap(int pid) : _pid(pid), _maps(pid), 
-                            _file_stream(open((std::string("/proc/") + std::to_string(_pid) + std::string("/pagemap")).c_str(), O_RDONLY)) {
-    
+Pagemap::Pagemap(int pid) : _pid(pid), _maps(pid),
+                            _file_stream(open((std::string("/proc/") + std::to_string(_pid)
+                                         + std::string("/pagemap")).c_str(), O_RDONLY)) {
     std::cout << _file_stream << std::endl;
 }
 
@@ -23,31 +23,32 @@ void Pagemap::read_pagemap_file() {
     std::cout << "Done reading maps." << std::endl;
 
     std::cout << "PAGE SIZE: " << PAGE_SIZE << std::endl;
-    
+
     for (auto range : _maps._maps_virtual_ranges) {
         for (uint64_t i = range.first; i < range.second; i += PAGE_SIZE) {
-            std::cout << "i: " << i << ", range: " << range.first << '-' << range.second << std::endl;
-            
+            // std::cout << "i: " << i << ", range: " << range.first << '-' << range.second << std::endl;
+
             uint64_t page_frame;
             uint64_t page_index = i / PAGE_SIZE * sizeof(page_frame);
 
-            std::cout << "page index: " << page_index << std::endl;
-            
             int ret = pread(_file_stream, &page_frame, sizeof(page_frame), page_index);
             if (ret == -1) {
                 exit(-1);
-            }
-            else if (ret == 0) {
-                std::cout << "EOF" << std::endl;    
-            }
-            else if (ret = sizeof(page_frame)) {
-                std::stringstream sstream;
-                sstream << std::hex << page_frame;
-                std::string result = sstream.str();
+            } else if (ret == 0) {
+                std::cout << "EOF" << std::endl;
+            } else if (ret = sizeof(page_frame)) {
+                std::stringstream page_index_stream;
+                page_index_stream << std::hex << i;
+                std::cout << "Virtual Page: 0x" << page_index_stream.str();
 
-                std::cout << "Page frame: 0x" << result << std::endl;
-            }
-            else {
+                bool page_present = page_frame & (static_cast<uint64_t>(1) << 63);
+                if (page_present) {
+                    std::stringstream page_frame_stream;
+                    page_frame_stream << std::hex << page_frame;
+                    std::cout << " -> " << "PFN: 0x" << page_frame_stream.str();
+                }
+                std::cout << std::endl;
+            } else {
                 exit(-2);
             }
         }
@@ -59,6 +60,6 @@ void Pagemap::read_pagemap_file() {
 void Pagemap::print_pagemap() {
     // _maps.print_maps();
     // std::cout << _pagemap_file << std::endl;
-    
+
     return;
 }
